d91d1930e19c47223c6d026a950285841b62a6b1
Danika Enad <danika.enad@gmail.com>
2023-02-09 17:49:37 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,43 @@
+
+const btnMode = document.querySelector('.btn-mode');
+const pictures = document.querySelectorAll('.picture');
+const picturesBox = document.querySelector('.pictures');
+const colorMode = document.querySelector('.color-mode')
+const root = document.documentElement;
+
+
+btnMode.addEventListener('click', function(){
+    document.body.classList.toggle('menu-open');
+})
+
+colorMode.addEventListener('change', function (e){
+    // console.log(e);
+
+    if (e.target.id == "color-picker"){
+        const bgc = e.target.value;
+        pictures.forEach(picture => {
+            picture.style.backgroundColor = bgc;
+        })
+    } else {
+        const blendMode = this.querySelector('input:checked').value;
+        root.style.setProperty('--bmode', blendMode);
+    }
+});
+
+pictures.forEach((picture) => {
+    picture.style.backgroundColor = randColor();
+});
+
+function randColor(){
+    function rand(){
+        return Math.floor(Math.random()*200);
+    }
+
+    const rgb = `rgb(${rand()}, ${rand()}, ${rand()})`;
+    return rgb;
+}
+
+// SECRET MESSAGES
+picturesBox.addEventListener('click', function(e){
+    e.target.parentElement.classList.toggle('message-up');
+})
